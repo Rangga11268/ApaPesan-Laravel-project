3d31413056dba236ef439188c857aff364b59810
Darell Rangga <darrelrangga@gmail.com>
2025-09-03 14:11:43 +0700

    Fix bug display

--- resources/js/Components/App/MessagaAttachments.jsx
+++ resources/js/Components/App/MessagaAttachments.jsx
@@ -9,63 +9,66 @@ const MessageAttachments = ({ attachments, attachmentClick }) => {
     return (
         <>
             {attachments.length > 0 && (
-                <div className="mt-2 flex flex-wrap justify-end gap-1">
+                <div
+                    className={`mt-2 flex flex-wrap justify-end gap-2 ${
+                        attachments.length === 1 ? "max-w-[300px]" : ""
+                    }`}
+                >
                     {attachments.map((attachment, ind) => (
                         <div
                             onClick={(ev) => attachmentClick(attachments, ind)}
                             key={attachment.id}
-                            className={
-                                `group flex flex-col items-center justify-center text-gray-500 relative cursor-pointer` +
-                                (isAudio(attachment)
-                                    ? "w-84"
-                                    : "w-32 aspect-square bg-blue-100")
-                            }
+                            className={`group relative flex justify-center items-center cursor-pointer overflow-hidden rounded-lg ${
+                                attachments.length > 1
+                                    ? "w-32 h-32"
+                                    : "w-full"
+                            }`}
                         >
                             {!isAudio(attachment) && (
                                 <a
                                     onClick={(ev) => ev.stopPropagation()}
                                     download
                                     href={attachment.url}
-                                    className="z-20 opacity-100 group-hover:opacity-100 transition-all w-8 h-8 flex
-                                    items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-0 top-0
-                                    cursor-pointer hover:bg-gray-800
-                                    "
+                                    className="z-20 opacity-0 group-hover:opacity-100 transition-all w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700/70 rounded-full absolute right-2 top-2 cursor-pointer hover:bg-gray-800"
                                 >
                                     <ArrowDownTrayIcon className="w-4 h-4" />
                                 </a>
                             )}
 
                             {isImage(attachment) && (
-                                <div className="relative flex justify-center items-center">
-                                    <img
-                                        src={attachment.url}
-                                        className="object-contain aspect-square"
-                                    />
-                                </div>
+                                <img
+                                    src={attachment.url}
+                                    className="w-full h-full object-cover"
+                                />
                             )}
                             {isVideo(attachment) && (
-                                <div className="relative flex justify-center items-center">
+                                <>
                                     <PlayCircleIcon className="z-20 absolute w-16 h-16 text-white opacity-70" />
-
                                     <div className="absolute left-0 top-0 w-full h-full bg-black/50 z-10"></div>
-                                    <video src={attachment.url}></video>
-                                </div>
+                                    <video
+                                        src={attachment.url}
+                                        className="w-full h-full object-cover"
+                                    ></video>
+                                </>
                             )}
                             {isAudio(attachment) && (
-                                <div className="relative flex justify-center items-center">
-                                    <audio
-                                        src={attachment.url}
-                                        controls
-                                    ></audio>
-                                </div>
+                                <audio
+                                    src={attachment.url}
+                                    controls
+                                    className="w-full"
+                                ></audio>
                             )}
                             {isPDF(attachment) && (
-                                <div className="relative flex justify-center items-center">
-                                    <div className="absolute left-0 top-0 right-0 bottom-0"></div>
+                                <div className="w-full h-full flex justify-center items-center bg-gray-800">
                                     <iframe
                                         src={attachment.url}
-                                        className="w-full h-full"
+                                        className="hidden"
                                     ></iframe>
+                                    <img
+                                        src="/img/pdf.png"
+                                        alt="PDF icon"
+                                        className="w-16 h-16"
+                                    />
                                 </div>
                             )}
                             {!isPreviewable(attachment) && (
@@ -73,10 +76,10 @@ const MessageAttachments = ({ attachments, attachmentClick }) => {
                                     onClick={(ev) => ev.stopPropagation()}
                                     href={attachment.url}
                                     download
-                                    className="flex flex-col justify-center items-center"
+                                    className="w-full h-full flex flex-col justify-center items-center bg-gray-800 p-2"
                                 >
                                     <PaperClipIcon className="w-10 h-10 mb-3" />
-                                    <small className="text-center">
+                                    <small className="text-center text-gray-300 break-all">
                                         {attachment.name}
                                     </small>
                                 </a>
